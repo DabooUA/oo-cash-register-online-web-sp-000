a131f48ae9d0ba27b5b980e0f820c93877dcb972
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-08 18:27:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 
   attr_accessor :discount, :total, :items, :last_transaction_amount
 
-  def initialize (discount = 20)
+  def initialize (discount = 0)
     @total = 0
     @discount = discount
     @items = []
