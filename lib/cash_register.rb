41e0e13b7310fa64d0fa88676ecc41591fb275b8
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-08 17:11:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,9 +26,9 @@ class CashRegister
     if @discount == 0
         return "There is no discount to apply."
       else
-      self.total_of_transaction = (0.01 * @discount * @total).to_i
+      self.total = (0.01 * @discount * @total).to_i
       #@total -= @total - (@total * @discount)
-     return "After the discount, the total comes to $#{@total_of_transaction}."
+     return "After the discount, the total comes to $#{@total}."
 
     end
 end
