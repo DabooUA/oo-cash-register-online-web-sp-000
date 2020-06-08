99d134a8936aa4c55db9e1512fb76d9237af2bed
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-08 16:47:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
 
   def apply_discount
     if @discount > 0
-       @discount = @discount / 100.to_f
+       @total = @total * (1 - @discount / 100).to_f
        @total -= @total - (@total * @discount)
       return "After the discount, the total comes to $#{@total.to_i}."
     else
